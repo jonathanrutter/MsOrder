ddd8607cf717a54cabc450837067dd00afa6e44e
jonathan rutter <jonathanrutter@gmail.com>
2024-11-25 21:29:30 +0000

    Replace Feign rest with Spring RestClient Add a circuit breaker using Resiliance4J

--- src/main/java/com/wuppski/microservices/ms_order/MsOrderApplication.java
+++ src/main/java/com/wuppski/microservices/ms_order/MsOrderApplication.java
@@ -2,10 +2,8 @@ package com.wuppski.microservices.ms_order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class MsOrderApplication {
 
 	public static void main(String[] args) {
